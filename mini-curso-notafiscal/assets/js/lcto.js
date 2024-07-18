cc8afd00a1bd572804cbf590b657146047044b4f
Fernando Barboza <fernando.p.barboza@gmail.com>
2024-07-18 15:13:49 -0300

    Lcto#23 Link Grupo WhatsApp#8

--- mini-curso-notafiscal/assets/js/lcto.js
+++ mini-curso-notafiscal/assets/js/lcto.js
@@ -10,7 +10,7 @@ const lcto = {
   lnk_cpl2: "https://www.youtube.com/embed/WAGBVnx6eAM",
   lnk_cpl3: "https://www.youtube.com/embed/EkxAXvLitfo",
   lnk_cpl4: "https://www.youtube.com/embed/MqARLBaUfKk",
-  lnk_whts: "https://chat.whatsapp.com/EOlQKNfCNfn1Hyrw1Y5BMa"
+  lnk_whts: "https://chat.whatsapp.com/J1VdopDUysTCdFxkFAusRT"
 };
 
 
