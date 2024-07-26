c5d391f645e5feefcebc3ac8ad7dc9429e3cc749
Fernando Barboza <fernando.p.barboza@gmail.com>
2024-07-26 06:35:21 -0300

    Lcto#23  Link Aula 3

--- mini-curso-notafiscal/assets/js/lcto.js
+++ mini-curso-notafiscal/assets/js/lcto.js
@@ -8,7 +8,7 @@ const lcto = {
   dt_abertura: "29/07",
   lnk_cpl1: "https://www.youtube.com/embed/bgGTNPCVwAY",
   lnk_cpl2: "https://www.youtube.com/embed/SG01i4Qcuu4",
-  lnk_cpl3: "https://www.youtube.com/embed/EkxAXvLitfo",
+  lnk_cpl3: "https://www.youtube.com/embed/fqqw8FPPWQ4",
   lnk_cpl4: "https://www.youtube.com/embed/MqARLBaUfKk",
   lnk_whts: "https://chat.whatsapp.com/J1VdopDUysTCdFxkFAusRT"
 };
