5362a378e1cc42c613e136867541762de649932c
fernandopbarboza <fernando.p.barboza@gmail.com>
2024-12-06 10:40:47 -0300

    Update-Login With Google

--- descomplicando-as-notas-fiscais/assets/js/lcto.js
+++ descomplicando-as-notas-fiscais/assets/js/lcto.js
@@ -76,3 +76,12 @@ const lnk_whts = document.getElementById("lnk_grupo_whatsapp")
 if (lnk_whts) {
   lnk_whts.href = lcto.lnk_whts;
 }
+
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
