cc2556038e95185e8beb32e5b3939746de842bf2
fernandopbarboza <fernando.p.barboza@gmail.com>
2025-01-27 01:10:12 -0300

    Lcto#27-Update-Roma

---
+++ expert-fiscal-3k-mais/assets/js/lcto.js
@@ -0,0 +1,10 @@
+const expert = {
+    tit_roma: "Ensino Contadores a <span style='color: #D0AB34;'>Dominarem </span> as <span style='color: #D0AB34;'>Notas Fiscais </span> com <span style='color: #D0AB34;'>Segurança </span> e estarem  <span style='color: #D0AB34;'>Prontos </span> para a <span style='color: #D0AB34;'>Reforma Tributária </span>",
+  };
+
+
+  //Roma / Promessa do Evento
+const roma = document.getElementById("roma_promessa")
+if (roma) {
+  roma_promessa.innerHTML = expert.tit_roma;
+}
