c822527f7cbf29d8070dfae3b387ed760c024161
Fernando Barboza <fernando.p.barboza@gmail.com>
2025-05-14 11:44:28 -0300

    Update-Contador-Expert-Captação de Leads em Planilha Google

--- contador-expert-em-notas-fiscais/assets/js/lcto.js
+++ contador-expert-em-notas-fiscais/assets/js/lcto.js
@@ -1,11 +1,11 @@
 const month_name = ["","Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 const lcto = {
-  dt_cpl1: "28/04",
-  dt_cpl2: "30/04",
-  dt_cpl3: "02/05",
-  dt_cpl4: "04/05",
-  dt_abertura: "04/11",
+  dt_cpl1: "09/06",
+  dt_cpl2: "11/06",
+  dt_cpl3: "13/06",
+  dt_cpl4: "15/06",
+  dt_abertura: "16/06",
   lnk_cpl1: "https://www.youtube.com/embed/Ts09R4hosnY", 
   lnk_cpl2: "https://www.youtube.com/embed/JTwWbU1rxO0", 
   lnk_cpl3: "https://www.youtube.com/embed/_GkF8hScT2A", 
@@ -22,10 +22,10 @@ const lcto = {
 const dt_ev = document.getElementById("datas_evento")
 if (dt_ev) {    
     // Datas do Evento - Ex.: 29 de Abril ao dia 05 de Maio
-    dt_ev.innerHTML = lcto.dt_cpl1.split("/")[0] + " de " + month_name[parseInt(lcto.dt_cpl1.split("/")[1])] + " à " +  lcto.dt_cpl4.split("/")[0] + " de " + month_name[parseInt(lcto.dt_cpl4.split("/")[1])] ;
+    //dt_ev.innerHTML = lcto.dt_cpl1.split("/")[0] + " de " + month_name[parseInt(lcto.dt_cpl1.split("/")[1])] + " à " +  lcto.dt_cpl4.split("/")[0] + " de " + month_name[parseInt(lcto.dt_cpl4.split("/")[1])] ;
     
     // Dias 24, 26 e 28 de Junho
-    //dt_ev.innerHTML = "Dias " + lcto.dt_cpl1.split("/")[0] + ", " + lcto.dt_cpl2.split("/")[0] + ", " + lcto.dt_cpl3.split("/")[0] + " e " +  lcto.dt_cpl4.split("/")[0] + " de " + month_name[parseInt(lcto.dt_cpl3.split("/")[1])] ;
+    dt_ev.innerHTML = "Dias " + lcto.dt_cpl1.split("/")[0] + ", " + lcto.dt_cpl2.split("/")[0] + ", " + lcto.dt_cpl3.split("/")[0] + " e " +  lcto.dt_cpl4.split("/")[0] + " de " + month_name[parseInt(lcto.dt_cpl3.split("/")[1])] ;
 }
 
 //Inclusão do link da aula 1
@@ -142,3 +142,35 @@ const phoneMask = (value) => {
   value = value.replace(/(\d)(\d{4})$/,"$1-$2")
   return value
 }
+
+/* Google-Sheet */
+const form = document.getElementById('form-inscricao');
+const responseMessage = document.getElementById('response-message');
+
+    form.addEventListener('submit', async (event) => {
+      event.preventDefault();
+
+      const formData = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        whatsapp: document.getElementById('whatsapp').value,
+        tag: document.getElementById('tag').value
+      };
+
+      try {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbzHdma_l-p9bJd39TaKxfZaFP-yeGs-H4TzcQjKCqHDJSG_fd8shRm6Nwi7sVzf8gFs/exec', {
+          method: 'POST',
+          body: JSON.stringify(formData)
+        });
+
+        if (response.ok) {
+          window.location.href = "https://www.expertfiscaltributario.com.br/contador-expert-em-notas-fiscais/obrigado/org/";
+          //responseMessage.textContent = 'Thank you! Your message has been sent.';
+          //form.reset();
+        } else {
+          responseMessage.textContent = 'Erro ao enviar a mensagem. Por favor, tente novamente.';
+        }
+      } catch (error) {
+        responseMessage.textContent = 'Ocorreu um erro ao enviar a mensagem. Por favor, tente novamente.';
+      }
+    });
