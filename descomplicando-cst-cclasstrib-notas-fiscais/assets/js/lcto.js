9891862968ed1a35b343fd8e54dfd8dc0a7053fd
Fernando Barboza <fernando.p.barboza@gmail.com>
2025-11-28 06:51:00 -0300

    Update-Descomplicando CST-Link redirect Oferta

--- descomplicando-cst-cclasstrib-notas-fiscais/assets/js/lcto.js
+++ descomplicando-cst-cclasstrib-notas-fiscais/assets/js/lcto.js
@@ -23,8 +23,8 @@ const lcto = {
   ,tit_cpl2: "O Método simplificado das Notas Fiscais para navegar nas Ondas da Reforma Tributária sem Erro"
   ,tit_cpl3: "Como Dominar Notas Fiscais e ficar um passo à Frente na Reforma Tributária"
   ,tit_cpl4: "Acesse o método avançado e torne-se Expert em Notas Fiscais atualizado com a Reforma Tributária"
-  //,lnk_redirect_aula: "window.location.href = 'https://www.expertfiscaltributario.com.br/expert-fiscal-3k-mais/matriculas-abertas/vip/';"
-  //,lnk_redirect: "window.location.href = 'https://www.expertfiscaltributario.com.br/expert-fiscal-3k-mais/matriculas-abertas/vip/';"
+  ,lnk_redirect_aula: "window.location.href = 'https://www.expertfiscaltributario.com.br/descomplicando-cst-cclasstrib-notas-fiscais/oferta/';"
+  //,lnk_redirect: "window.location.href = 'https://www.expertfiscaltributario.com.br/descomplicando-cst-cclasstrib-notas-fiscais/oferta/';"
 };
 
 const dt_ev = document.getElementById("datas_evento")
