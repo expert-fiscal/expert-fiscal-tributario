3b7c0fec60fbdea5d9478ba1f68f1614b4390f45
Fernando Barboza <fernando.p.barboza@gmail.com>
2026-02-27 16:22:29 -0300

    Update-ExtensaoUniversitaria-Precificação-Hotmart

--- extensao-universitaria-em-conformidade-fiscal/assets/js/main.js
+++ extensao-universitaria-em-conformidade-fiscal/assets/js/main.js
@@ -228,3 +228,16 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+  /* Hotmart Widget */
+	function importHotmart(){ 
+ 		var imported = document.createElement('script'); 
+ 		imported.src = 'https://static.hotmart.com/checkout/widget.min.js'; 
+ 		document.head.appendChild(imported); 
+		var link = document.createElement('link'); 
+		link.rel = 'stylesheet'; 
+		link.type = 'text/css'; 
+		link.href = 'https://static.hotmart.com/css/hotmart-fb.min.css'; 
+		document.head.appendChild(link);	} 
+ 	importHotmart(); 
+  /* Hotmart Widget */
